d490cf53c6e3a43365ad90fcdedc33028ae698cf
ijones16 <jones58ian@gmail.com>
2020-09-27 15:01:09 -0400

    add 09 comment mutation end code

---
+++ 09-urqlCommentMutation/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {AuthContext} from './contexts/AuthContext'
+import Comments from './components/Comments'
+import Input from './components/Input'
+
+function App() {
+  const {login, status} = React.useContext(AuthContext)
+
+  if (!status || !status.github) {
+    return (
+      <div>
+        <h1>Log in to github</h1>
+        <p>In order to see your profile, you'll have to log in with Github.</p>
+        <button onClick={() => login('github')}>Log in with Github</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Comments />
+          <Input />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
