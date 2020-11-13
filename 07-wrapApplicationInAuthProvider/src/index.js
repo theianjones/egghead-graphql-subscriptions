b6df214c3eb6f2e28679c036ecbe07095e6f7695
ijones16 <jones58ian@gmail.com>
2020-11-13 10:00:59 -0500

    Split up old lesson 4 code

---
+++ 07-wrapApplicationInAuthProvider/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {createClient, Provider} from 'urql'
+import {CLIENT_URL, auth} from './utils/auth'
+import {AuthProvider} from './contexts/AuthContext'
+
+const client = createClient({
+  url: CLIENT_URL,
+  fetchOptions: {
+    headers: {...auth.authHeaders()},
+  },
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider auth={auth}>
+      <Provider value={client}>
+        <App />
+      </Provider>
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
